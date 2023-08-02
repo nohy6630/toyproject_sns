27f4cbe349b8c4a757c4854f3fa83a2a391968bd
nohy6630 <nohy6630@gmail.com>
2023-08-02 14:48:27 +0900

    feat: first commit

---
+++ src/main/java/com/yenoh/toyproject_sns/ToyprojectSnsApplication.java
@@ -0,0 +1,13 @@
+package com.yenoh.toyproject_sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprojectSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyprojectSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yenoh/toyproject_sns/ToyprojectSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yenoh.toyproject_sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyprojectSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
